17b05497328a2323d37f221614927705adafef78
Chiara Paci <chiara.paci@gmail.com>
2016-11-28 16:42:50 +0100

    First dummy view

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -2,6 +2,8 @@ import os
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+import unittest
+
 BASE_DIR   = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(BASE_DIR)
 
@@ -10,13 +12,45 @@ os.environ["PATH"]+=":"+GECKODRIVER_BIN
 
 FIREFOX_PATH = "/usr/local/firefox/firefox"
 
-browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
-    firefox_path=FIREFOX_PATH
-))
+class NewVisitorTest(unittest.TestCase):  
+
+    def setUp(self):  
+        self.browser = webdriver.Firefox(firefox_binary=FirefoxBinary(firefox_path=FIREFOX_PATH))
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Writing', self.browser.title)  
+        self.fail('Finish the test!')  
+
+        # She is invited to enter a scene (title) straight away
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a scene list
+
+        # There is still a text box inviting her to add another scene. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
 
 
-#browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'Django' in browser.title
-
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')  
