77aa44bea7a31d1830dba040fc8922bc80b703d5
Chiara Paci <chiara.paci@gmail.com>
2016-12-06 14:52:25 +0100

    test_my_sections: precreate sessions, move login checks into base

--- costruttoridimondi/functional_tests/test_login.py
+++ costruttoridimondi/functional_tests/test_login.py
@@ -43,16 +43,12 @@ class LoginTest(base.FunctionalTest):
         time.sleep(self.wait_time)
 
         # she is logged in!
-        self.browser.find_element_by_link_text('Log out')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.assert_logged_in(email=TEST_EMAIL)
 
         # Now she logs out
         self.browser.find_element_by_link_text('Log out').click()
         time.sleep(self.wait_time)
 
         # She is logged out
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
-        self.browser.find_element_by_name('email')
+        self.assert_logged_out(email=TEST_EMAIL)
         time.sleep(self.wait_time)
